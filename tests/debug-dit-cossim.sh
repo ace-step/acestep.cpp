94f328fe50cb5aefe1f190142f4c5558f1ba09ea
Pascal <admin@serveurperso.com>
2026-03-01 15:36:51 +0100

    Tests

--- tests/debug-dit-cossim.sh
+++ tests/debug-dit-cossim.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-./debug-dit-cossim.py --mode both --quant BF16 > BF16.log
-./debug-dit-cossim.py --mode both --quant Q8_0 > Q8_0.log
-./debug-dit-cossim.py --mode both --quant Q6_K > Q6_K.log
-./debug-dit-cossim.py --mode both --quant Q5_K_M > Q5_K_M.log
-./debug-dit-cossim.py --mode both --quant Q4_K_M > Q4_K_M.log
+cd ..
+./build.sh
+cd tests
+./debug-dit-cossim.py --mode turbo --quant BF16 > CUDA-BF16.log
+./debug-dit-cossim.py --mode turbo --quant Q8_0 > CUDA-Q8_0.log
+./debug-dit-cossim.py --mode turbo --quant Q6_K > CUDA-Q6_K.log
+./debug-dit-cossim.py --mode turbo --quant Q5_K_M > CUDA-Q5_K_M.log
+./debug-dit-cossim.py --mode turbo --quant Q4_K_M > CUDA-Q4_K_M.log
+
+cd ..
+./buildvulkan.sh
+cd tests
+./debug-dit-cossim.py --mode turbo --quant BF16 > Vulkan-BF16.log
+./debug-dit-cossim.py --mode turbo --quant Q8_0 > Vulkan-Q8_0.log
+./debug-dit-cossim.py --mode turbo --quant Q6_K > Vulkan-CPU_Q6_K.log
+./debug-dit-cossim.py --mode turbo --quant Q5_K_M > Vulkan-Q5_K_M.log
+./debug-dit-cossim.py --mode turbo --quant Q4_K_M > Vulkan-Q4_K_M.log
+
+cd ..
+./buildcpu.sh
+cd tests
+./debug-dit-cossim.py --mode turbo --quant BF16 > CPU-BF16.log
+./debug-dit-cossim.py --mode turbo --quant Q8_0 > CPU-Q8_0.log
+./debug-dit-cossim.py --mode turbo --quant Q6_K > CPU-Q6_K.log
+./debug-dit-cossim.py --mode turbo --quant Q5_K_M > CPU-Q5_K_M.log
+./debug-dit-cossim.py --mode turbo --quant Q4_K_M > CPU-Q4_K_M.log
