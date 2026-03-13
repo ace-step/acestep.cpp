1ccd28a219cb46a6bf8dce1221440bb6d17486c8
Pascal <admin@serveurperso.com>
2026-03-13 17:05:57 +0100

    build: Android Termux

--- buildtermux.sh
+++ buildtermux.sh
@@ -4,5 +4,6 @@ rm -rf build
 mkdir build
 cd build
 
-cmake .. -DGGML_CPU_ALL_VARIANTS=ON -DGGML_VULKAN=ON -DGGML_BACKEND_DL=ON
+cmake .. -DGGML_CPU_ALL_VARIANTS=ON -DGGML_VULKAN=ON -DGGML_BACKEND_DL=ON \
+         -DVulkan_LIBRARY=/system/lib64/libvulkan.so
 cmake --build . --config Release -j "$(nproc)"
